fa1e82855a06a10d441e3a1f1be79b4a38e98072
Kevin Miller <keveemiller@gmail.com>
2020-06-15 15:20:24 -0600

    chore: Fix sidebar

--- components/layout.js
+++ components/layout.js
@@ -9,36 +9,38 @@ const { Header, Footer, Sider, Content } = Layout
 
 export default ({ title, children }) => (
   <Layout>
-    <Sider id="sidebar">
-      <img src="/logo.svg" alt="Covid tracking project" />
-      <Dropdown
-        trigger={['click']}
-        overlay={
-          <Menu>
-            <Menu.Item>
-              <Link href="website/snapshots">
-                <a>CTP website snapshots</a>
-              </Link>
-            </Menu.Item>
-          </Menu>
-        }
-      >
-        <Button>
-          More options <DownOutlined />
-        </Button>
-      </Dropdown>
-      <h2>States</h2>
-      <Menu>
-        {states
-          .sort((a, b) => (a.name > b.name ? 1 : -1))
-          .map((state) => (
-            <Menu.Item key={state.state}>
-              <Link href={`/state/${state.state.toLowerCase()}`}>
-                <a>{state.name}</a>
-              </Link>
-            </Menu.Item>
-          ))}
-      </Menu>
+    <Sider>
+      <div id="sidebar">
+        <img src="/logo.svg" alt="Covid tracking project" />
+        <Dropdown
+          trigger={['click']}
+          overlay={
+            <Menu>
+              <Menu.Item>
+                <Link href="website/snapshots">
+                  <a>CTP website snapshots</a>
+                </Link>
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          <Button>
+            More options <DownOutlined />
+          </Button>
+        </Dropdown>
+        <h2>States</h2>
+        <Menu>
+          {states
+            .sort((a, b) => (a.name > b.name ? 1 : -1))
+            .map((state) => (
+              <Menu.Item key={state.state}>
+                <Link href={`/state/${state.state.toLowerCase()}`}>
+                  <a>{state.name}</a>
+                </Link>
+              </Menu.Item>
+            ))}
+        </Menu>
+      </div>
     </Sider>
     <Layout>
       <Content
