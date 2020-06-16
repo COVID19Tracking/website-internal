f3d5237c8311548c91eda38514ff84351fa5eeca
Kevin Miller <keveemiller@gmail.com>
2020-06-16 16:32:24 -0600

    chore: Force login

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Layout, Menu, Dropdown, Button } from 'antd'
 import netlifyIdentity from 'netlify-identity-widget'
 import { DownOutlined } from '@ant-design/icons'
@@ -10,6 +10,7 @@ const { SubMenu } = Menu
 const { Header, Footer, Sider, Content } = Layout
 
 export default ({ title, children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
   useEffect(() => {
     if (typeof window === 'undefined') {
       return
@@ -18,56 +19,64 @@ export default ({ title, children }) => {
     const user = netlifyIdentity.currentUser()
     if (!user) {
       netlifyIdentity.open()
+    } else {
+      setIsLoggedIn(true)
     }
   }, [])
 
   return (
     <Layout>
-      <Sider>
-        <div id="sidebar">
-          <img src="/logo.svg" alt="Covid tracking project" />
-          <Dropdown
-            trigger={['click']}
-            overlay={
+      {isLoggedIn ? (
+        <>
+          <Sider>
+            <div id="sidebar">
+              <img src="/logo.svg" alt="Covid tracking project" />
+              <Dropdown
+                trigger={['click']}
+                overlay={
+                  <Menu>
+                    <Menu.Item>
+                      <Link href="/website/history">
+                        <a>CTP website history</a>
+                      </Link>
+                    </Menu.Item>
+                  </Menu>
+                }
+              >
+                <Button>
+                  More options <DownOutlined />
+                </Button>
+              </Dropdown>
+              <h2>States</h2>
               <Menu>
-                <Menu.Item>
-                  <Link href="/website/history">
-                    <a>CTP website history</a>
-                  </Link>
-                </Menu.Item>
+                {states
+                  .sort((a, b) => (a.name > b.name ? 1 : -1))
+                  .map((state) => (
+                    <Menu.Item key={state.state}>
+                      <Link href={`/state/${state.state.toLowerCase()}`}>
+                        <a>{state.name}</a>
+                      </Link>
+                    </Menu.Item>
+                  ))}
               </Menu>
-            }
-          >
-            <Button>
-              More options <DownOutlined />
-            </Button>
-          </Dropdown>
-          <h2>States</h2>
-          <Menu>
-            {states
-              .sort((a, b) => (a.name > b.name ? 1 : -1))
-              .map((state) => (
-                <Menu.Item key={state.state}>
-                  <Link href={`/state/${state.state.toLowerCase()}`}>
-                    <a>{state.name}</a>
-                  </Link>
-                </Menu.Item>
-              ))}
-          </Menu>
-        </div>
-      </Sider>
-      <Layout>
-        <Content
-          className="site-layout-background"
-          style={{
-            padding: 24,
-            margin: 0,
-            minHeight: 280,
-          }}
-        >
-          {children}
-        </Content>
-      </Layout>
+            </div>
+          </Sider>
+          <Layout>
+            <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+            >
+              {children}
+            </Content>
+          </Layout>
+        </>
+      ) : (
+        <></>
+      )}
     </Layout>
   )
 }
