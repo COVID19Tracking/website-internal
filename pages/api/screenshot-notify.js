1c40b7a9e14bf2731e056cec34f85419b9ca1b32
Kevin Miller <keveemiller@gmail.com>
2021-01-11 14:05:14 -0800

    chore: Post to slack when new manual screenshot is posted

---
+++ pages/api/screenshot-notify.js
@@ -0,0 +1,56 @@
+import { WebClient } from '@slack/web-api'
+import { DateTime } from 'luxon'
+import validateCookie from '../../lib/validate-cookie'
+
+const channel = 'C015E7CR35X'
+
+export default async (req, res, context) => {
+  if (!validateCookie(req)) {
+    res.statusCode = 403
+    res.end('Login required')
+    return
+  }
+
+  const { covidUser } = req.cookies
+  covidUser.split(':')
+  const user = covidUser[0]
+
+  const startThread = `--- Manual Screenshots ${DateTime.local().toFormat(
+    'LL/dd/yy',
+  )} ---`
+  const web = new WebClient(process.env.COVID_INTERNAL_SLACK_TOKEN)
+  const convos = await web.conversations.history({
+    channel,
+    oldest: DateTime.local().minus({ days: 5 }).toMillis() / 1000,
+  })
+  let threadTimestamp = false
+  convos.messages.forEach((message) => {
+    if (message.text.search(startThread) > -1) {
+      threadTimestamp = message.ts
+    }
+  })
+  if (!threadTimestamp) {
+    const message = await web.chat.postMessage({
+      channel,
+      text: startThread,
+    })
+    threadTimestamp = message.message.ts
+  }
+  web.chat.postMessage({
+    channel,
+    unfurl_links: false,
+    unfurl_media: false,
+    thread_ts: threadTimestamp,
+    text: `<${req.body.screenshot}|${req.body.state} - ${req.body.dataType} - ${
+      req.body.coreDataType
+    }>
+Date & time: ${DateTime.fromISO(req.body.dateTime)
+      .setZone('America/New_York')
+      .toFormat('LL/dd/yy ttt')}
+Uploaded by: <@${user}>
+    `,
+  })
+  console.log()
+  res.statusCode = 200
+  res.json({ done: true })
+}
